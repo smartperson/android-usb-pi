3dee220ea12064afda34d039e9e728a69c84891e
Varun Mehta <varun@mehtasw.com>
2014-07-27 22:14:19 -0400

    adding call to android-usb-control as part of phone docking script it should work…

--- phone_docked.sh
+++ phone_docked.sh
@@ -2,3 +2,4 @@
 
 /root/android-usb-pi/android-usb-audio.py $1 $2
 (sleep 3s ; pactl load-module module-loopback source=`pactl list sources short | grep alsa_input.usb | cut -f 1`) &
+/root/android-usb-pi/android-usb-control.py
